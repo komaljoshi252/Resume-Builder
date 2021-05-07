42d2c46e5d0e096e85cf4de8744d8b84b1dfc4d8
komaljoshi252 <55447267+komaljoshi252@users.noreply.github.com>
2021-05-07 14:24:17 +0530

    Add files via upload

---
+++ UserRegistration.java
@@ -0,0 +1,185 @@
+import java.awt.BorderLayout;
+import java.awt.Container;
+import java.awt.EventQueue;
+import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+import javax.swing.border.EmptyBorder;
+
+public class UserRegistration extends JFrame {
+    private static final long serialVersionUID = 1L;
+    private JPanel contentPane;
+    private JTextField firstname;
+    private JTextField lastname;
+    private JTextField email;
+    private JTextField username;
+    private JTextField mob;
+    private JPasswordField passwordField;
+    private JButton btnNewButton;
+    private Container c; 
+
+    /**
+     * Launch the application.
+     */
+    public static void main(String[] args) {
+        EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                try {
+                    UserRegistration frame = new UserRegistration();
+                    frame.setVisible(true);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    /**
+     * Create the frame.
+     */
+
+    public UserRegistration() {
+        setIconImage(Toolkit.getDefaultToolkit().getImage("C:\\\\Users\\\\joshi_f9n026d\\\\Downloads\\\\pexels-miguel-á-padriñán-255379.jpg"));
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setBounds(50, 10, 1014, 597);
+        setResizable(false);
+        
+        setLayout(new BorderLayout());
+    	setContentPane(new JLabel(new ImageIcon("C:\\\\Users\\\\joshi_f9n026d\\\\Downloads\\\\pexels-miguel-á-padriñán-255379.jpg")));
+    	setLayout(new FlowLayout());
+    	
+    	c = getContentPane(); 
+        c.setLayout(null);;
+
+        JLabel lblNewUserRegister = new JLabel("New User Register");
+        lblNewUserRegister.setFont(new Font("Times New Roman", Font.PLAIN, 42));
+        lblNewUserRegister.setBounds(362, 52, 325, 50);
+        c.add(lblNewUserRegister);
+
+        JLabel lblName = new JLabel("First name");
+        lblName.setFont(new Font("Tahoma", Font.PLAIN, 20));
+        lblName.setBounds(58, 152, 99, 43);
+        c.add(lblName);
+
+        JLabel lblNewLabel = new JLabel("Last name");
+        lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 20));
+        lblNewLabel.setBounds(58, 243, 110, 29);
+        c.add(lblNewLabel);
+
+        JLabel lblEmailAddress = new JLabel("Email\r\n address");
+        lblEmailAddress.setFont(new Font("Tahoma", Font.PLAIN, 20));
+        lblEmailAddress.setBounds(58, 324, 124, 36);
+        c.add(lblEmailAddress);
+
+        firstname = new JTextField();
+        firstname.setFont(new Font("Tahoma", Font.PLAIN, 32));
+        firstname.setBounds(214, 151, 228, 50);
+        c.add(firstname);
+        firstname.setColumns(10);
+
+        lastname = new JTextField();
+        lastname.setFont(new Font("Tahoma", Font.PLAIN, 32));
+        lastname.setBounds(214, 235, 228, 50);
+        c.add(lastname);
+        lastname.setColumns(10);
+
+        email = new JTextField();
+
+        email.setFont(new Font("Tahoma", Font.PLAIN, 32));
+        email.setBounds(214, 320, 228, 50);
+        c.add(email);
+        email.setColumns(10);
+
+        username = new JTextField();
+        username.setFont(new Font("Tahoma", Font.PLAIN, 32));
+        username.setBounds(707, 151, 228, 50);
+        c.add(username);
+        username.setColumns(10);
+
+        JLabel lblUsername = new JLabel("Username");
+        lblUsername.setFont(new Font("Tahoma", Font.PLAIN, 20));
+        lblUsername.setBounds(542, 159, 99, 29);
+        c.add(lblUsername);
+
+        JLabel lblPassword = new JLabel("Password");
+        lblPassword.setFont(new Font("Tahoma", Font.PLAIN, 20));
+        lblPassword.setBounds(542, 245, 99, 24);
+        c.add(lblPassword);
+
+        JLabel lblMobileNumber = new JLabel("Mobile number");
+        lblMobileNumber.setFont(new Font("Tahoma", Font.PLAIN, 20));
+        lblMobileNumber.setBounds(542, 329, 139, 26);
+        c.add(lblMobileNumber);
+
+        mob = new JTextField();
+        mob.setFont(new Font("Tahoma", Font.PLAIN, 32));
+        mob.setBounds(707, 320, 228, 50);
+        c.add(mob);
+        mob.setColumns(10);
+
+        passwordField = new JPasswordField();
+        passwordField.setFont(new Font("Tahoma", Font.PLAIN, 32));
+        passwordField.setBounds(707, 235, 228, 50);
+        c.add(passwordField);
+
+        btnNewButton = new JButton("Register");
+        btnNewButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                String firstName = firstname.getText();
+                String lastName = lastname.getText();
+                String emailId = email.getText();
+                String userName = username.getText();
+                String mobileNumber = mob.getText();
+                int len = mobileNumber.length();
+                String password = passwordField.getText();
+
+                String msg = "" + firstName;
+                msg += " \n";
+                if (len != 10 || firstname.getText()=="" || lastname.getText()=="" || email.getText()=="" || username.getText()=="") {
+                    JOptionPane.showMessageDialog(btnNewButton, "Enter a valid value");
+                }
+                else {
+
+                try {
+                    Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/dbname", "root", "Password");
+                 // connecting to database using jdbc driver
+                    //using "jdbc:mysql://localhost:3306/dbname","username", "password"
+                    //where username is by default "root" and password is the one set by you during mysql installation
+
+                    String query = "INSERT INTO account values('" + firstName + "','" + lastName + "','" + userName + "','" +
+                        password + "','" + emailId + "','" + mobileNumber + "')";
+
+                    Statement sta = connection.createStatement();
+                    int x = sta.executeUpdate(query);
+                    if (x == 0) {
+                        JOptionPane.showMessageDialog(btnNewButton, "This is alredy exist");
+                    } else {
+                        SwingExample b = new SwingExample(firstName,mobileNumber,emailId);
+                        b.setVisible(true);
+                    }
+                    connection.close();
+                } catch (Exception exception) {
+                    exception.printStackTrace();
+                }
+            }
+            }
+        });
+        btnNewButton.setFont(new Font("Tahoma", Font.PLAIN, 22));
+        btnNewButton.setBounds(399, 447, 259, 74);
+        c.add(btnNewButton);
+    }
+}
